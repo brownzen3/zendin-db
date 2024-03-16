939bec7f662ede3cd3270e916ceab7c19329e75e
Zen <87526909+brownzen3@users.noreply.github.com>
2024-03-16 12:40:35 +0000

    tidy up

--- drone-content-db.js
+++ drone-content-db.js
@@ -1,10 +1,13 @@
-// id - url/post[id]
-// type - 0: image, 1: video, 2: kuula
-// path - https://drive.google.com/file/d/[path] or https://kuula.co/share/[path]
+// https://drone.zendin.co.uk
 
+
+// Last upload date
+const upload_date = "Jun, 2023";
+
+// Posts (one array per location, one array in media[i][2] per file)
 const media = [
-// [ ID, "title", [ [TYPE, "path", "altitude", "date", "resolution"] ] ],
 
+// [ ID, "title", [ [TYPE, "path", "altitude", "date", "resolution"] ] ],
 [ 5, "Kingston upon Thames, England", [ [1, "1C4AQXNmic_dvrdrtNOKrKcUJ2dt6r7Of", "200m", "5 Jan, 2024", "HD 30fps"] ] ],
 [ 4, "Milton Keynes, England",        [ [2, "5pVH6", "500m", "11 Jun, 2023", "210MP"] ] ],
 [ 3, "Willen Lake, England",          [ [0, "path", "210m", "1 May, 2023", "12MP"], [1, "1C4AQXNmic_dvrdrtNOKrKcUJ2dt6r7Of", "100-500m", "1 May, 2023", "4K 30fps"] ] ],
@@ -12,3 +15,12 @@ const media = [
 [ 1, "Isle of Wight, England",        [ [2, "5pV2K", "80m", "19 Aug, 2021", "210MP"] ] ],
 
 ];
+
+// ITEMS:
+// id    -  html id = "#post[id]"
+// title -  header of post (location)
+//      type  -  0: image, 1: video, 2: kuula
+//      path  -  https://drive.google.com/file/d/[path] or https://kuula.co/share/[path]
+//      altitude (AGL)
+//      date (d mmm, yyyy)
+//      resolution (MP / res, FPS)
