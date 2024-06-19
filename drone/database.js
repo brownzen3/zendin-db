79b0037e8d3af0337847727aff8aa3079b82d1d4
Zen <87526909+brownzen3@users.noreply.github.com>
2024-06-19 19:41:39 +0100

    Update database.js

--- drone/database.js
+++ drone/database.js
@@ -9,5 +9,3 @@ const database = {
     "0453": ["https://live.staticflickr.com/65535/53797937503_db0c40ba27_o.jpg", "https://live.staticflickr.com/65535/53802212665_e0e354e654_o.jpg", "Apr 2022", "Dead Sea, IL"       , -70 , 120 , "210"],
     "0338": ["https://live.staticflickr.com/65535/53798129525_ccc8c76776_o.jpg", "https://live.staticflickr.com/65535/53800839107_b9531212dd_o.jpg", "Aug 2022", "Isle of Wight, UK"  , -70 , 45  , "210"],
 };
-
-<a data-flickr-embed="true" data-vr="true" href="https://www.flickr.com/gp/200879779@N02/D0r026zFFY" title="1270-lo"><img src="" width="2048" height="1024" alt="1270-lo"/></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
