b3c6d28920304bb287269ba3fffa15484f7486c8
Zen <87526909+brownzen3@users.noreply.github.com>
2024-06-19 09:25:32 +0100

    +drone db

---
+++ drone/database.js
@@ -0,0 +1,10 @@
+const allowLoad = true;
+const compatibleImporter = "1.0";
+
+const database = {
+//  "ID  ": ["Image URL                                                       ", "Low-resolution Image URL                                        ", "Mm  YYYY", "Location, XY       ",  yaw, n.o.],
+    "1050": ["https://live.staticflickr.com/65535/53797630011_8f56e57363_o.jpg", "https://live.staticflickr.com/65535/53800721460_7e05bc40c4_o.jpg", "Jun 2024", "Kempston, UK"       , -70 , 60  ],
+    "1000": ["https://live.staticflickr.com/65535/53797938918_5ea46c9b14_o.jpg", "___LINK_________________________________________________________", "Jun 2023", "Milton Keynes, UK"  , -70 , 60  ],
+    "0453": ["https://live.staticflickr.com/65535/53797937503_db0c40ba27_o.jpg", "___LINK_________________________________________________________", "Apr 2022", "Dead Sea, IL"       , -70 , 120 ],
+    "0338": ["https://live.staticflickr.com/65535/53798129525_ccc8c76776_o.jpg", "___LINK_________________________________________________________", "Aug 2022", "Isle of Wight, UK"  , -70 , 60  ],
+}
