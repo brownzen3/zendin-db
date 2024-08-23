dd2a6de2a93c6b386081f66d4924c0e3b9b24102
Zen <87526909+brownzen3@users.noreply.github.com>
2024-08-23 09:28:31 +0100

    Update database.js

--- drone/database.js
+++ drone/database.js
@@ -6,9 +6,10 @@ const compatibleImporter = "1.0";
 const database = {
 //  "ID  ": ["Image URL                                                       ", "Low-resolution Image URL                                        ", "Mm  YYYY", "Location, XY       ",  yaw, n.o.,  "MP"  "(darkMode Image URL)                                            ", "(darkMode Low-resolution Image URL)                             "],
     "2635": ["https://live.staticflickr.com/65535/53941003558_651f60d1f2_o.jpg", "https://live.staticflickr.com/65535/53941017083_2b61df7ee1_o.jpg", "Aug 2024", "Whitby, UK"         , -70 , 195 ,  "89"],
+    "2593": ["https://live.staticflickr.com/65535/53941295055_34a7bcd0da_o.jpg", "https://live.staticflickr.com/65535/53940841616_fbd7c7b3f4_o.jpg", "Aug 2024", "Whitby, UK",          -70 , 0   ,  "89"],
     "2050": ["https://live.staticflickr.com/65535/53797630011_8f56e57363_o.jpg", "https://live.staticflickr.com/65535/53800721460_7e05bc40c4_o.jpg", "Jun 2024", "Kempston, UK"       , -70 , 60  ,  "89"], //"https://live.staticflickr.com/65535/53806902957_c6ec0986d0_o.jpg", "https://live.staticflickr.com/65535/53808160814_76f61ea629_o.jpg"],
     "1270": ["https://live.staticflickr.com/65535/53802191781_0223581d00_o.jpg", "https://live.staticflickr.com/65535/53802434098_6a457149df_o.jpg", "Aug 2023", "Karnataka, IN"      , -70 , 0   , "181"],
     "1000": ["https://live.staticflickr.com/65535/53797938918_5ea46c9b14_o.jpg", "https://live.staticflickr.com/65535/53802018823_c7afd1c1b6_o.jpg", "Jun 2023", "Milton Keynes, UK"  , -70 , 170 , "210"],
     "0453": ["https://live.staticflickr.com/65535/53797937503_db0c40ba27_o.jpg", "https://live.staticflickr.com/65535/53802212665_e0e354e654_o.jpg", "Apr 2022", "Dead Sea, IL"       , -70 , 120 , "210"],
-    "0338": ["https://live.staticflickr.com/65535/53798129525_ccc8c76776_o.jpg", "https://live.staticflickr.com/65535/53800839107_b9531212dd_o.jpg", "Aug 2022", "Isle of Wight, UK"  , -70 , 45  , "210"],
+    "0338": ["https://live.staticflickr.com/65535/53798129525_ccc8c76776_o.jpg", "https://live.staticflickr.com/65535/53800839107_b9531212dd_o.jpg", "Aug 2022", "Isle of Wight, UK"  , -70 , 45  , "210"] ,
 };
