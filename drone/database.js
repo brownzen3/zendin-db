9c153bc9577a4ca310316c5a8998e90c16d9cfac
Zen <87526909+brownzen3@users.noreply.github.com>
2024-08-29 10:09:14 +0100

    Update database.js

--- drone/database.js
+++ drone/database.js
@@ -11,7 +11,7 @@ const database = {
     "2478": ["https://live.staticflickr.com/65535/53952652897_a526a32f2e_o.jpg", "https://live.staticflickr.com/65535/53953544391_b855ed3503_o.jpg", "Jul 2024", "Coorg, IN"          , -70 , 0   ,  "89"],
     "2050": ["https://live.staticflickr.com/65535/53797630011_8f56e57363_o.jpg", "https://live.staticflickr.com/65535/53800721460_7e05bc40c4_o.jpg", "Jun 2024", "Kempston, UK"       , -70 , 60  ,  "89"], //"https://live.staticflickr.com/65535/53806902957_c6ec0986d0_o.jpg", "https://live.staticflickr.com/65535/53808160814_76f61ea629_o.jpg"],
     "1270": ["https://live.staticflickr.com/65535/53802191781_0223581d00_o.jpg", "https://live.staticflickr.com/65535/53802434098_6a457149df_o.jpg", "Aug 2023", "Karnataka, IN"      , -70 , 0   , "181"],
-    "1000": ["https://live.staticflickr.com/65535/53797938918_5ea46c9b14_o.jpg", "https://live.staticflickr.com/65535/53802018823_c7afd1c1b6_o.jpg", "Jun 2023", "Milton Keynes, UK"  , -70 , 170 , "210"],
+  //"1000": ["https://live.staticflickr.com/65535/53797938918_5ea46c9b14_o.jpg", "https://live.staticflickr.com/65535/53802018823_c7afd1c1b6_o.jpg", "Jun 2023", "Milton Keynes, UK"  , -70 , 170 , "210"],
     "0453": ["https://live.staticflickr.com/65535/53797937503_db0c40ba27_o.jpg", "https://live.staticflickr.com/65535/53802212665_e0e354e654_o.jpg", "Apr 2022", "Dead Sea, IL"       , -70 , 120 , "210"],
     "0338": ["https://live.staticflickr.com/65535/53798129525_ccc8c76776_o.jpg", "https://live.staticflickr.com/65535/53800839107_b9531212dd_o.jpg", "Aug 2022", "Isle of Wight, UK"  , -70 , 45  , "210"],
 };
