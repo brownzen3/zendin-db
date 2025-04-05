c6d6bf491a19f6fc798a71f8662d4d46e4ac3cde
Zen <87526909+brownzen3@users.noreply.github.com>
2025-04-05 08:29:27 +0100

    Update database.js

--- drone/database.js
+++ drone/database.js
@@ -5,7 +5,7 @@ const compatibleImporter = "1.0";
 const database = {
 //  "ID__":  ["ImageURL________________________________________________________", "lowResImageURL__________________________________________________",  "Mmm_YYYY",  "Location, AA"       ,  yaw , nrth ,  "MP"  ],    "darkModeImageURL________________________________________________", "darkModeLowResImageURL__________________________________________"],
 
-    "3731":  ["https://live.staticflickr.com/65535/54431062489_8ce48f68cd_f.jpg", "https://live.staticflickr.com/65535/54431239850_822046466f_f.jpg",  "Dec 2024",  "Sliema, MT"         ,  -70 ,  0   , "181"  ],
+    "3731":  ["https://live.staticflickr.com/65535/54431062489_584ebabd35_o.jpg", "https://live.staticflickr.com/65535/54431239850_0d73821829_o.jpg",  "Dec 2024",  "Sliema, MT"         ,  -70 ,  0   , "181"  ],
     "2635":  ["https://live.staticflickr.com/65535/53941003558_651f60d1f2_o.jpg", "https://live.staticflickr.com/65535/53941017083_2b61df7ee1_o.jpg",  "Aug 2024",  "Whitby, UK"         ,  -70 ,  170 ,  "89",       "https://live.staticflickr.com/65535/53941295055_34a7bcd0da_o.jpg", "https://live.staticflickr.com/65535/53940841616_fbd7c7b3f4_o.jpg"],
     "2555":  ["https://live.staticflickr.com/65535/53953787548_b28a48a553_o.jpg", "https://live.staticflickr.com/65535/53953534561_d806cb88fc_o.jpg",  "Aug 2024",  "Karnataka, IN"      ,  -70 ,  0   ,  "89"  ],
     "2478":  ["https://live.staticflickr.com/65535/53952652897_a526a32f2e_o.jpg", "https://live.staticflickr.com/65535/53953544391_b855ed3503_o.jpg",  "Jul 2024",  "Coorg, IN"          ,  -70 ,  0   ,  "89"  ],
